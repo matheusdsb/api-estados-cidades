0f29dc1302ba9a56a669aad2513ba4bb59bf450c
Matheus Sabino <matheusdsb@gmail.com>
2020-11-10 22:52:44 -0300

    Componente de tabela - links

--- app/src/app/components/table/column-iinterface.ts
+++ app/src/app/components/table/column-iinterface.ts
@@ -1,4 +1,11 @@
+export interface LinkColumn {
+    titulo: string;
+    resolveLink: (row) => string;
+}
 export interface TableColumn {
     id: string;
     label: string;
+    tipo?: 'valor' | 'link';
+    resolve?: (row) => string;
+    links?: LinkColumn[];
 }
